2d72d83a0237dc0f7add174efcdca21ead565cb7
emaillistverify <contact@emaillistverify.com>
2016-08-22 15:27:28 +0100

    Update emailverify.py

--- emailverify.py
+++ emailverify.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python 
 # _*_ coding:utf-8 _*_
-__author__ = "Eugen K"
-__email__ = "eugen_k69@yahoo.com"
 
 
 import requests
@@ -13,7 +11,7 @@ class EmailListVerifyOne():
     def __init__(self, key, email):
         self.key = key
         self.email = email
-        self.verif = "https://app.emaillistverify.com/api/verifEmail?secret="
+        self.verif = "https://apps.emaillistverify.com/api/verifEmail?secret="
         self.url = self.verif+self.key+"&email="+self.email
 
     
@@ -29,7 +27,7 @@ class EmailListVerifyBulk():
         self.key = key
         self.name = 'File' + datenow.strftime("%Y-%m-%d %H:%M")
         self.user_file = user_file
-        self.url = 'https://app.emaillistverify.com/api/verifApiFile?secret='+key+'&filename=%s' % self.name
+        self.url = 'https://apps.emaillistverify.com/api/verifApiFile?secret='+key+'&filename=%s' % self.name
 
 
     def upload(self):
@@ -53,7 +51,7 @@ class EmailListVerifyBulk():
        
         with open('id_file','r') as f:
             ids = f.read()
-        url = 'https://app.emaillistverify.com/api/getApiFileInfo?secret='+self.key+'&id=%s' % ids
+        url = 'https://apps.emaillistverify.com/api/getApiFileInfo?secret='+self.key+'&id=%s' % ids
         r = requests.get(url)
         with open('result.txt', 'a') as res:
             res.write(r.content+'\n')
